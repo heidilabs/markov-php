3d2afe051a16d2cb23acb3b4f8c12d81ebf8735c
Erika Heidi <erika.christina@gmail.com>
2014-12-16 15:18:06 +0100

    its working

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ markov-php
 ==========
 
 experiments with markov chain in php
+
+
+##usage
+Just clone and run `composer install` to create the autoloader. Refer to the "demo.php" file for an example.
+
+
+        <?php
+             
+        #sample text, number of words to chain. bigger the sampler, more randomness!
+        $chain = new MarkovPHP\WordChain($sample, 2);
+        
+        #blocks to generate (in this case, 10 x 2 words)
+        $sentence = $chain->generate(10);
+        
+        echo "YOUR PIECE OF WISDOM FROM NIETZSCHE<br><br>";
+        echo $sentence;
+        
---
+++ src/MarkovPHP/WordChain.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace MarkovPHP;
+
+class WordChain
+{
+    /** @var  string Content to use as base */
+    protected $sample;
+
+    /** @var  int Number of words to chain */
+    protected $chain;
+
+    /** @var array Words from sample */
+    protected $words;
+
+    /**
+     * @param string $sample
+     * @param int $chain
+     */
+    public function __construct($sample, $chain = 2)
+    {
+        $this->sample = $sample;
+        $this->words = $this->splitText($sample, $chain);
+    }
+
+    /**
+     * @param int $blocks
+     * @return string
+     */
+    public function generate($blocks = 10)
+    {
+        return $this->makeChain($this->getRandom(), $blocks);
+    }
+
+    /**
+     * @return string
+     */
+    public function getRandom()
+    {
+        $startIndex = array_rand($this->words);
+
+        return $this->words[$startIndex];
+    }
+
+    /**
+     * @param string $sentence
+     * @param int $blocks
+     * @return string
+     */
+    public function makeChain($sentence, $blocks = 10)
+    {
+        $lastCouple = $sentence;
+
+        for ($i=1; $i<=$blocks; $i++) {
+
+            $complement = $this->findMatch($lastCouple);
+
+            if (!$complement) {
+                $complement = $this->getRandom();
+            }
+
+            $sentence .= ' ' . $complement;
+            $lastCouple = $complement;
+        }
+
+        return $sentence;
+    }
+
+    /**
+     * @param $string
+     * @return string|null
+     */
+    public function findMatch($string)
+    {
+        $search = array_keys($this->words, $string);
+        if (count($search)) {
+            $index = $search[array_rand($search)] + 1;
+
+            return $this->words[$index];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $text
+     * @param int $chain
+     * @return array
+     */
+    public function splitText($text, $chain)
+    {
+        $words = preg_split("/[\s]+/", $text);
+
+        $chunks = array_chunk($words, $chain);
+        $split = [];
+
+        foreach ($chunks as $chunk) {
+            $split[] = implode(' ', $chunk);
+        }
+
+        return $split;
+    }
+}
