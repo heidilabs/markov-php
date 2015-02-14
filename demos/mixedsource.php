af6efc1e73aec880a498aee429a76516e3a7e9eb
Erika Heidi <erika.christina@gmail.com>
2015-02-14 15:16:57 +0100

    organizing demos and resources

---
+++ demos/mixedsource.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$sample1 = file_get_contents(__DIR__ . '/resources/nietzsche.txt');
+$sample2 = file_get_contents(__DIR__ . '/resources/mobydick.txt');
+
+$chain = new \MarkovPHP\MixedSourceChain($sample1, $sample2);
+$sentence = $chain->generate();
+
+echo "RESULT:<br>";
+echo $sentence;
+
+
--- demos/wordchaintheme.php
+++ demos/wordchaintheme.php
@@ -1,11 +1,9 @@
 <?php
-
-require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 $sample = file_get_contents(__DIR__ . '/resources/nietzsche.txt');
 
 $chain = new MarkovPHP\WordChain($sample, 2);
-
 $theme = "hate";
 $sentence = $chain->generate(50, $theme);
 
